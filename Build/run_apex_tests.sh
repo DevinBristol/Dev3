633943322ce5d92a01bb54652bff336fff8ea316
Devin <devinobrien17@gmail.com>
2024-04-10 04:40:47 -0500

    commit runapextests file change

--- Build/run_apex_tests.sh
+++ Build/run_apex_tests.sh
@@ -15,9 +15,9 @@
 #sfdx force:mdapi:deploy --checkonly -u DevHub -d test_code/ -w -1 -l RunLocalTests
 
 echo "Deploying source to org"
-sf project deploy start --source-dir DevTest3 --target-org DevHub
+sf project deploy start --source-dir DevTest3
 
 echo "Testing code in org"
-sf apex run test --test-level RunLocalTests --outputdir test-results --result-format tap --target-org DevHub
+sf apex run test --test-level RunLocalTests --outputdir test-results --result-format tap
 
 C:\Users\Devin\IdeaProjects\DevTest3\src
