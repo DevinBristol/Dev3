fb856a618878e2296946406bc36debb7a32e3457
Devin <devinobrien17@gmail.com>
2024-04-10 02:06:34 -0500

    commit update sandbox app key

--- Build/login_to_sandbox.sh
+++ Build/login_to_sandbox.sh
@@ -2,7 +2,7 @@
 
 echo "Logging into Sandbox Org"
 mkdir keys
-echo $Sandbox_CERT_KEY | base64 -di > keys/server.key
+echo $SANDBOX_CERT_KEY | base64 -di > keys/server.key
 
 echo "Authenticating Org"
-sfdx force:auth:jwt:grant --clientid $Sandbox_APP_KEY --jwtkeyfile keys/server.key --username $SANDBOX_USERNAME --setdefaultdevhubusername -a Devhub
+sfdx force:auth:jwt:grant --clientid $SANDBOX_APP_KEY --jwtkeyfile keys/server.key --username $SANDBOX_USERNAME --setdefaultdevhubusername -a Devhub
