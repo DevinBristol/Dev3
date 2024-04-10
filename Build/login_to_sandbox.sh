7882459a4c1b37c657b0b1b081838708509e1a36
Devin <devinobrien17@gmail.com>
2024-04-10 02:19:18 -0500

    commit rando changes 1

--- Build/login_to_sandbox.sh
+++ Build/login_to_sandbox.sh
@@ -5,4 +5,4 @@ mkdir keys
 echo $SANDBOX_CERT_KEY | base64 -di > keys/server.key
 
 echo "Authenticating Org"
-sfdx force:auth:jwt:grant --clientid $SANDBOX_APP_KEY --jwtkeyfile keys/server.key --username $SANDBOX_USERNAME --setdefaultdevhubusername -a Devhub
+sfdx force:auth:jwt:grant --clientid $SANDBOX_APP_KEY --jwtkeyfile keys/server.key --username $SANDBOX_USERNAME --setdefaultdevhubusername -a DevHub
--- Build/run_apex_tests.sh
+++ Build/run_apex_tests.sh
@@ -1,5 +1,5 @@
 echo "Deploying source to org"
-sfdx force:source:deploy --sourcepath src --targetusername DevHub
+sfdx force:source:deploy --sourcepath force-app --targetusername DevHub
 
 echo "Testing code in org"
 sfdx force:apex:test:run --testlevel runLocalTests --outputdir test-results --resultformat tap --targetusername DevHub
